39691e16f095f43dfe5b8a1cf0d8232d40d812d5
Marcelo Ian <marceloian5@gmail.com>
2021-11-23 23:10:46 -0300

    adiciona pista1 e pista2

---
+++ works/pistas/pista1.js
@@ -0,0 +1,13 @@
+
+const pista1 = [
+    [1, 1, 3, 1, 1],
+    [1, 0, 0, 0, 1],
+    [3, 0, 0, 0, 3],
+    [1, 0, 0, 0, 1],
+    [1, 1, 2, 1, 1]
+];
+// 0 : fora da pista
+// 1 : pista comum
+// 2 : ponto de largada/chegada
+// 3 : checkpoint
+export {pista1};
---
+++ works/pistas/pista2.js
@@ -0,0 +1,12 @@
+const pista2 = [
+    [1, 1, 3, 0, 0],
+    [1, 0, 1, 0, 0],
+    [3, 0, 1, 1, 3],
+    [1, 0, 0, 0, 1],
+    [1, 1, 2, 1, 1]
+];
+// 0 : fora da pista
+// 1 : pista comum
+// 2 : ponto de largada/chegada
+// 3 : checkpoint
+export {pista2};
